aa0f9de550fa1a611bf743a8dccc7dba9a90b405
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2024-06-19 15:58:16 -0500

    Add issue

--- src/Infrastructure/dummy.js
+++ src/Infrastructure/dummy.js
@@ -1,1 +1,10 @@
-i=0;
+function checkEqual(a, b) {
+    if (a == b) { // Noncompliant: using non-strict equality '=='
+      return "Equal";
+    } else {
+      return "Not equal";
+    }
+  }
+  
+  console.log(checkEqual(0, false)); // Output: "Equal"
+  
